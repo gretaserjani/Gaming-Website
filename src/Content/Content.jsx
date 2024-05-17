66369417523d3aee08f11a836d7a53bc45bbef43
gretaserjani <greta.serjani@gmail.com>
2024-05-17 16:50:55 +0200

    Hoqa foton

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -8,9 +8,7 @@ const Content = () => {
         <h1>Discover a world</h1>
         <span className="second-line">OF ADVENTURE</span>
         <div>Unlock the Next Level of Fun! Dive into a world of excitement <br></br> and adventure with our 
-            thrilling gaming website. <br></br> Play, compete, and conquer today!</div>
-        <img className="cat" src={homepage} />
-        
+            thrilling gaming website. <br></br> Play, compete, and conquer today!</div>        
         
     </container>
   )
